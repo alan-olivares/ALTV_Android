28f26269d9d0a920f361ad9ae4e665d212a6a7ae
alan-olivares <31354812+alan-olivares@users.noreply.github.com>
2020-10-17 12:49:39 -0500

    Update LoginActivity.java

--- app/src/main/java/com/example/alanolivares/altv/LoginActivity.java
+++ app/src/main/java/com/example/alanolivares/altv/LoginActivity.java
@@ -58,40 +58,21 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends AppCompatActivity{
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
     private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
     Boolean as=true;
-    Button boton,reg;
+    Button reg;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Configure el formulario de inicio de sesión.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -160,11 +141,8 @@ public class LoginActivity extends AppCompatActivity{
 
 
     private void attemptLogin() {
-        // Restablecer errores.
         mEmailView.setError(null);
         mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
 
@@ -278,10 +256,6 @@ public class LoginActivity extends AppCompatActivity{
                         editor.putString("contra",contra);
                         editor.putString("nombre",jsonObject.getString("name"));
                         editor.commit();
-                        System.out.println(jsonObject.getString("usuario"));
-                        System.out.println(jsonObject.getString("password"));
-                        System.out.println(correo);
-                        System.out.println(contra);
                         a=true;
                         Toast.makeText(getApplicationContext(), "Inicio de sesión satisfactorio", Toast.LENGTH_SHORT).show();
                         Intent re = new Intent(LoginActivity.this, MenuLateral.class);
@@ -293,12 +267,6 @@ public class LoginActivity extends AppCompatActivity{
                             .make(findViewById(R.id.root_layout), "Correo o contraseña incorrecta",Snackbar.LENGTH_LONG)
                             .show();
                 }
-                    /*listViewContactos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                    }
-                });*/
             } catch (JSONException e) {
                 e.printStackTrace();
             }
